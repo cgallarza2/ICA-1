607523e37a12b7c4a40cf94723ca1f0a1fb58c60
Jukish <jaketdemail@gmail.com>
2024-09-20 23:14:41 -0700

    spelling errors

--- printMainMenu.cpp
+++ printMainMenu.cpp
@@ -14,8 +14,8 @@ void printMainMenu(){
 	cout << setw(45) << "*" << setw(54) << "Main Menu" << "*" << endl;
 	cout << setw(SCREEN_WIDTH - 1) << "*" << "*" << endl;
 	cout << setw(11) << "*" << setw(88) << "1. Access existing Checking Account" << "*" << endl;
-	cout << setw(11) << "*" << setw(88) << "2. Access exisitng Savings Account" << "*" << endl;
-	cout << setw(11) << "*" << setw(88) << "3. Access exisitng Certificate of Deposit" << "*" << endl;
+	cout << setw(11) << "*" << setw(88) << "2. Access existing Savings Account" << "*" << endl;
+	cout << setw(11) << "*" << setw(88) << "3. Access existing Certificate of Deposit" << "*" << endl;
 	cout << setw(11) << "*" << setw(88) << "4. Exit" << "*" << endl;
 	cout << setw(SCREEN_WIDTH - 1) << "*" << "*" << endl;
 	cout << right;
